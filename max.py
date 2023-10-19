eb1009e759a60d2abf21ce5e61374d4e8efc838d
seatlle <148451568+seatlle@users.noreply.github.com>
2023-10-19 12:52:35 +0100

    Add files via upload

---
+++ max.py
@@ -0,0 +1,7 @@
+print('entrez votre premier nombre')
+nbre1 = float(input())
+nbre2 = float(input('entrez votre deuxiéme nombre '))
+if nbre1>nbre2 :
+    print("votre plus grand est :",nbre1)
+else :
+    print("votre plus grand est :",nbre2)
